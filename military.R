6c531307fd59dbcf1d38132e655ef78cc5f2c55a
fahadbinthabit <fahadbinthabit@gmail.com>
2020-10-09 03:06:04 +0300

    added plots and pivot longer added weapons per country

--- military.R
+++ military.R
@@ -25,45 +25,153 @@ library(maps)
 library(ggrepel)
 library(HSAUR)
 library(dplyr)
+library(scales)
+library(rgeos)
+library(classInt)
+library(RColorBrewer)
+library(rworldmap)
 theme_set(theme_bw())
 
 
+
+# Data set
+
 mimo <- read_csv("GlobalFirePower.csv")
 
-glimpse(mimo)
 
 
 
+#top ten military Army in terms of  Military personnel , Tanks , etc
+
+
+scipen(9000000)
 head(mimo[order(mimo$`Total Military Personnel`, decreasing=TRUE), ], 10) %>%
   select(Country,
-         `Total Military Personnel`) %>% ggplot(aes(Country,`Total Military Personnel`)) + geom_col()
+         `Total Military Personnel`) %>% ggplot(aes(Country,`Total Military Personnel`)) + geom_col() + coord_flip()
 
 head(mimo[order(mimo$`Fighter Aircraft`, decreasing=TRUE), ], 10) %>%
   select(Country,
-         `Fighter Aircraft`) %>% ggplot(aes(Country,`Fighter Aircraft`)) + geom_col()
+         `Fighter Aircraft`) %>% ggplot(aes(Country,`Fighter Aircraft`)) + geom_col() + coord_flip()
 
 head(mimo[order(mimo$`Attack Helicopters`, decreasing=TRUE), ], 10) %>%
   select(Country,
-         `Attack Helicopters`) %>% ggplot(aes(Country,`Total Military Personnel`)) + geom_col()
+         `Attack Helicopters`) %>% ggplot(aes(Country,`Total Military Personnel`)) + geom_col() + coord_flip()
 
 
 head(mimo[order(mimo$`Combat Tanks`, decreasing=TRUE), ], 10) %>%
   select(Country,
-         `Combat Tanks`) %>% ggplot(aes(Country,`Combat Tanks`)) + geom_col()
+         `Combat Tanks`) %>% ggplot(aes(Country,`Combat Tanks`)) + geom_col() + coord_flip()
 
 
 head(mimo[order(mimo$`Submarines`, decreasing=TRUE), ], 10) %>%
   select(Country,
-         `Submarines`) %>% ggplot(aes(Country,`Submarines`)) + geom_col()
+         `Submarines`) %>% ggplot(aes(Country,`Submarines`)) + geom_col() + coord_flip()
 
 
 head(mimo[order(mimo$`Rocket Projectors`, decreasing=TRUE), ], 10) %>%
   select(Country,
-         `Rocket Projectors`) %>% ggplot(aes(Country,`Rocket Projectors`)) + geom_col()
+         `Rocket Projectors`) %>% ggplot(aes(Country,`Rocket Projectors`)) + geom_col() + coord_flip()
 
 head(mimo[order(mimo$`Destroyers`, decreasing=TRUE), ], 10) %>%
   select(Country,
          `Destroyers`) %>% ggplot(aes(Country,`Destroyers`)) + geom_col() + coord_flip()
+
+
+
+head(mimo[order(mimo$`Destroyers`, decreasing=TRUE), ], 10) %>%
+  select(Country,
+         `Destroyers`) %>% ggplot(aes(Country,`Destroyers`)) + geom_col() + coord_flip()
+
+
+
+
+
+# weapons per country # !!! < change country in filter > !!!!
+
+
+mimo <- mimos %>%
+  select(Country,`Fighter Aircraft`,`Attack Aircraft`,`Transport Aircraft`,`Attack Helicopters`,`Combat Tanks`,`Armored Fighting Vehicles`,`Self-Propelled Artillery`,`Towed Artillery`,`Rocket Projectors`, `Total Naval Assets`, `Aircraft Carriers`, Frigates, Destroyers, Corvettes, Submarines, `Patrol Craft`, `Mine Warfare Vessels`) 
+
+mimos_ %>%
+  pivot_longer(-Country, names_to = "Weapons" , values_to = "value") %>%
+  filter(Country == "Russia") %>% 
+  ggplot(aes(Weapons, value)) + geom_col() + coord_flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+# gene world map
+ggplot(data = world) +
+  geom_sf() +
+  labs( x = "Longitude", y = "Latitude") +
+  ggtitle("World map", subtitle = paste0("(", length(unique(world$admin)), " countries)"))
+
+world
+
+
+mp_map <- c( mimo$Country, mimo$ISO3, mimo$`Total Military Personnel`)
+                               
+inthemap <- joinCountryData2Map(mimo, 
+                                  joinCode = "ISO3",
+                                  nameJoinColumn = "Country")
+
+mapParams <- mapCountryData(inthemap, 
+                            nameColumnToPlot= "Total Military Personnel",
+                            oceanCol = "azure2",
+                            catMethod = "categorical",
+                            missingCountryCol = gray(.8),
+                           # colourPalette = c("coral",
+                                             # "coral2",
+                                           #   "coral3", "orangered", 
+                                           #   "orangered3", "orangered4"),
+                            addLegend = F,
+                            mapTitle = "",
+                            border = NA)
+# add legend and display map
+do.call(addMapLegendBoxes, c(mapParams,
+                             x = 'bottom',
+                             title = "No. of visits",
+                             horiz = TRUE,
+                             bg = "transparent",
+                             bty = "n"))
+
+
+
 
 
 
