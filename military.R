b09db1e0dc330497d522964dcde2818104eb3cba
fahadbinthabit <fahadbinthabit@gmail.com>
2020-10-09 23:28:28 +0300

    Var and SD are added to aircraft strength

--- military.R
+++ military.R
@@ -35,12 +35,12 @@ theme_set(theme_bw())
 
 
 # Data set
-
-mimo <- read_csv("GlobalFirePower.csv")
-
-
+mimo1 <- read_csv("GlobalFirePower.csv")
+mimo <- na.omit(mimo1)
 
 
+
+na.omit(mimo)
 #top ten military Army in terms of  Military personnel , Tanks , etc
 
 
@@ -84,6 +84,26 @@ head(mimo[order(mimo$`Destroyers`, decreasing=TRUE), ], 20) %>%
 
 
 
+##### var and SD (NA removed from results)
+
+globalaircraftstrength_mean <- mimo %>%
+  summarise(avg = sum(`Total Aircraft Strength`)/length(`Total Aircraft Strength`))
+
+globalaircraftstrength_mean$avg
+
+gfp_as1 <- mimo$`Total Aircraft Strength`[mimo$`Total Aircraft Strength`]
+gfp_as <- na.omit(gfp_as1) 
+
+globalaircraftstrength_mean <- sum(gfp_as)/length(gfp_as)
+
+
+gfp_as_var <- sum((gfp_as - globalaircraftstrength_mean)^2)/(length(gfp_as) - 1)
+var(gfp_as)
+
+
+gfp_as_sd <- sqrt(gfp_as_var)
+sd(gfp_as)
+
 
 
 # weapons per country # !!! < change country in filter > !!!!
@@ -108,6 +128,64 @@ mimo %>%
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mimospider <- mimo %>% 
+  select(Country, `Total Aircraft Strength`,`Total Helicopter Strength`,`Merchant Marine Strength`) %>%
+  pivot_longer(-Country, names_to = "type", values_to ="strenght" ) %>%
+  filter(Country == "Saudi Arabia") 
+
+library(fmsb)
+
+data <- as.data.frame(matrix( sample( mimospider$strenght , 10 , replace=T) , ncol=100000))
+ 
+# Create data: note in High school for Jonathan:
+
+colnames(data) <- mimospider$type
+
+data <- rbind(rep(20,10) , rep(0,100) , data)
+
+
+# To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
+data <- rbind(rep(100000,10) , rep(0,100) , data)
+
+radarchart( data  , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,1000,100), cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+# Check your data, it has to look like this!
+# head(data)
+
+# The default radar chart 
+radarchart(data)
 
 
 
