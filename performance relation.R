f320954e0eb419e02f9cc09e65b572b8490b10d6
fahadbinthabit <fahadbinthabit@gmail.com>
2020-10-15 11:46:07 +0300

    performance relation - requests ~ errors  | delay linear ploting

---
+++ performance relation.R
@@ -0,0 +1,27 @@
+#find the relation between error column and the rest #aggregator report 
+#technical performance 
+
+library(tidyverse)
+library(rio)
+library(tinytex)
+library(ggplot2)
+library(data.table)
+
+
+main_ = read_csv("tameeni report/arranged data.csv")
+main_ = as.data.table(main_)
+
+fit =  lm(late_success ~ sample_count, main_ )
+predict(fit)
+summary(fit)
+
+ggplot(main_ , aes(x= sample_count , y= late_success)) + geom_point() + geom_smooth(method = "lm")  
+
+
+fit =  lm(sample_count ~ Error_Response , main_ )
+predict(fit)
+summary(fit)
+
+ggplot(main_ , aes(x= Error_Response , y=sample_count )) + geom_point() + geom_smooth(method = "lm")  
+
+
